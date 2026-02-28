53c962619c698140c2c24bd7389e1b06e14beb72
Kato-30 <trandinhan19112003@gmail.com>
2026-02-28 19:47:36 +0700

    connect DB and server

---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+const dotenv = require('dotenv');
+const db = require('./config/database');
+const app = require('./app');
+
+dotenv.config({
+    path: './.env',
+});
+
+
+const startServer = async () => {
+    try {
+        await db.connect();
+        app.on('error', (error) => {
+            console.error('Error starting server:', error);
+            throw error;
+        });
+        app.listen(process.env.PORT || 3000, () => {
+            console.log(`Server is running on port: ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.error('MongoDB connection failed:', error);
+    }
+};
+
+startServer();
